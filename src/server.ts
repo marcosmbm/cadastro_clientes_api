b919e345df6a1493ea83cb2107c81a5b6de8543a
marcosmbm <marmbmti@gmail.com>
2023-03-11 23:39:44 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import BaseRouter from './routes/v1/Base';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/v1', BaseRouter);
 
